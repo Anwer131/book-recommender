697ab3704841fefcc8bff702ebb1898cf96216d5
Munazzil Anwer <munazzil131@gmail.com>
2024-11-16 12:57:40 +0530

    added 3 more db

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,36 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import pandas as pd
+import pickle
 import numpy as np
 import os
 
+# Initialize Flask app
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Allow Cross-Origin requests
 
 # Load the datasets
-popular_data_path = os.path.join(os.path.dirname(__file__), 'popular.pkl')
-popular_df = pd.read_pickle(popular_data_path)
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
 
-books_data_path = os.path.join(os.path.dirname(__file__), 'books.pkl')
-pt_data_path = os.path.join(os.path.dirname(__file__), 'pt.pkl')
-similarity_data_path = os.path.join(os.path.dirname(__file__), 'similarity_scores.pkl')
-
-books = pd.read_pickle(books_data_path)
-pt = pd.read_pickle(pt_data_path)
-similarity_scores = pd.read_pickle(similarity_data_path)
-
-# API endpoint to get popular books
+# API endpoint to fetch popular books
 @app.route('/api/books', methods=['GET'])
 def get_books():
-    books = popular_df.to_dict(orient='records')
-    return jsonify(books)
+    """Returns a list of popular books"""
+    data = {
+        "book_name": list(popular_df['Book-Title'].values),
+        "author": list(popular_df['Book-Author'].values),
+        "image": list(popular_df['Image-URL-M'].values),
+        "votes": list(popular_df['num_ratings'].values),
+        "rating": list(popular_df['avg_rating'].values)
+    }
+    return jsonify(data)
 
-# API endpoint for book recommendations
+# API endpoint to get book recommendations
 @app.route('/api/recommend_books', methods=['POST'])
 def recommend_books():
+    """Returns book recommendations based on user input"""
     data = request.json
     user_input = data.get('user_input')
 
@@ -35,17 +38,20 @@ def recommend_books():
     if user_input not in pt.index:
         return jsonify({"message": "Book not found. Please try another title.", "data": []}), 404
 
+    # Get the index of the input book
     index = np.where(pt.index == user_input)[0][0]
     similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
 
+    # Prepare recommendation data
     recommendations = []
     for i in similar_items:
-        item = {}
         temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item['title'] = list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values)[0]
-        item['author'] = list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values)[0]
-        item['image'] = list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values)[0]
-        recommendations.append(item)
+        book_info = {
+            "title": list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values)[0],
+            "author": list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values)[0],
+            "image": list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values)[0]
+        }
+        recommendations.append(book_info)
 
     return jsonify({"data": recommendations})
 
